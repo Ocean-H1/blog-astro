837b683a52fa37201ae267955300dd4cdb14ee38
OceanH <1404939185@qq.com>
2024-07-28 18:51:29 +0800

    style: code format

--- src/config.ts
+++ src/config.ts
@@ -9,23 +9,24 @@ import { LinkPreset } from './types/config'
 export const siteConfig: SiteConfig = {
   title: 'Ocean Han',
   subtitle: 'Blog',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  lang: 'en', // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false, // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: 'assets/images/demo-banner.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
   },
-  favicon: [    // Leave this array empty to use the default favicon
+  favicon: [
+    // Leave this array empty to use the default favicon
     // {
     //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     // }
-  ]
+  ],
 }
 
 export const navBarConfig: NavBarConfig = {
@@ -35,14 +36,14 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'Star',
-      url: 'https://github.com/Ocean-H1/blog-astro',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
+      url: 'https://github.com/Ocean-H1/blog-astro', // Internal links should not include the base path, as it is automatically added
+      external: true, // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/avatar.jpg', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Ocean Han',
   bio: '先完成，后完美！',
   links: [
@@ -53,14 +54,14 @@ export const profileConfig: ProfileConfig = {
     },
     {
       name: 'BiLiBiLi',
-      icon: 'fa6-brands:bilibili',       
+      icon: 'fa6-brands:bilibili',
       url: 'https://space.bilibili.com/382688944?spm_id_from=333.1007.0.0',
     },
     {
       name: 'GitHub',
-      icon: 'fa6-brands:github',    // Visit https://icones.js.org/ for icon codes
-                                    // You will need to install the corresponding icon set if it's not already included
-                                    // `pnpm add @iconify-json/<icon-set-name>`
+      icon: 'fa6-brands:github', // Visit https://icones.js.org/ for icon codes
+      // You will need to install the corresponding icon set if it's not already included
+      // `pnpm add @iconify-json/<icon-set-name>`
       url: 'https://github.com/Ocean-H1',
     },
   ],
