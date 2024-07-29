d95d56c97d53b07546cab3eb9a8d9dc7eb754808
OceanH <1404939185@qq.com>
2024-07-30 00:38:17 +0800

    feat: 更换首页banner

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,7 @@ export const siteConfig: SiteConfig = {
   },
   banner: {
     enable: true,
-    src: 'assets/images/demo-banner.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: 'assets/images/banner1.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
   },
   favicon: [
