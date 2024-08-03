1d7b8f891fcb2608532331cfd16f66889342525b
OceanH <1404939185@qq.com>
2024-08-03 13:08:06 +0800

    feat: live2d相关资源目录调整

--- src/live2d-widget/autoload.js
+++ src/live2d-widget/autoload.js
@@ -35,7 +35,8 @@ if (screen.width >= 768) {
     initWidget({
       waifuPath: live2d_path + 'waifu-tips.json',
       //apiPath: "https://live2d.fghrsh.net/api/",
-      cdnPath: 'https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/',
+      // cdnPath: 'https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/',
+      cdnPath: '/live2d_api/',
       tools: [
         'hitokoto',
         'asteroids',
