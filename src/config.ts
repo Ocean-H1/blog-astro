8768e993efa92b45abfc27fb69cdf63aa5c118ba
OceanH <1404939185@qq.com>
2024-08-03 17:35:50 +0800

    feat: update bio

--- src/config.ts
+++ src/config.ts
@@ -42,7 +42,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.jpg', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Ocean Han',
-  bio: '先完成，后完美！',
+  bio: 'Done is better than perfect.',
   links: [
     {
       name: 'Homepage',
